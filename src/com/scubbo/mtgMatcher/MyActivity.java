8c6aa65202411ff83c6d66bd163e5f669de35f32
Jack Jackson <jackjack@amazon.com>
2014-06-27 22:33:02 +0100

    Checks for play services

--- src/com/scubbo/mtgMatcher/MyActivity.java
+++ src/com/scubbo/mtgMatcher/MyActivity.java
@@ -1,9 +1,16 @@
 package com.scubbo.mtgMatcher;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class MyActivity extends Activity {
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +18,36 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        TextView mDisplay = (TextView) findViewById(R.id.loggerView);
+
+        Context context = getApplicationContext();
+
+        if (checkPlayServices()) {
+            mDisplay.setText("you have play services");
+        } else {
+            mDisplay.setText("you do not have play services");
+        }
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                String TAG="THIS IS A TAG";
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
